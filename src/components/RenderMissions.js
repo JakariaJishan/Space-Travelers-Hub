e019043a94ab0bd51476da4bf986d273735120e1
JakariaJishan <jakariajishan987@gmail.com>
2023-03-15 01:56:34 +0600

    Add conditional components

--- src/components/RenderMissions.js
+++ src/components/RenderMissions.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React from "react";
 
-function RenderMissions({mission, handleJoin, handleLeave}) {
-    const {mission_id, mission_name} = mission
+function RenderMissions({ mission, handleJoin, handleLeave }) {
+  const { mission_id, mission_name, reserved } = mission;
   return (
     <div>
-        id:{mission_id} |
-        mission name: {mission_name}
-        <button type='button' onClick={()=> handleJoin(mission_id)}>Join Mission</button>
-        <button type='button' onClick={()=> handleLeave(mission_id)}>Leave Mission</button>
+      id:{mission_id} | mission name: {mission_name}
+      {reserved ? <b>Active Member</b> : <b>NOT A MEMBER</b>}
+      {reserved ? (
+        <button type="button" onClick={() => handleLeave(mission_id)}>
+          Leave Mission
+        </button>
+      ) : (
+        <button type="button" onClick={() => handleJoin(mission_id)}>
+          Join Mission
+        </button>
+      )}
     </div>
-  )
+  );
 }
 
-export default RenderMissions
+export default RenderMissions;
