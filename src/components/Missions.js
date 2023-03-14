0ac720f1ebcf5dab975fcd448ae6b29cc1d7dfa5
JakariaJishan <jakariajishan987@gmail.com>
2023-03-15 01:46:55 +0600

    Implement mission leaving

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchMissions, joinMissions } from "../app/missions/missionsSlice";
+import { fetchMissions, joinMissions, leaveMissions } from "../app/missions/missionsSlice";
 import RenderMissions from "./RenderMissions";
 
 function Missions() {
@@ -12,13 +12,21 @@ function Missions() {
   }, [dispatch]);
 
   const handleJoin = (id) => {
-    dispatch(joinMissions(id))
-  }
+    dispatch(joinMissions(id));
+  };
+  const handleLeave = (id) => {
+    dispatch(leaveMissions(id));
+  };
 
   return (
     <div>
       {missions.map((mission) => (
-        <RenderMissions key={mission.mission_id} mission={mission} handleJoin={handleJoin}/>
+        <RenderMissions
+          key={mission.mission_id}
+          mission={mission}
+          handleJoin={handleJoin}
+          handleLeave={handleLeave}
+        />
       ))}
     </div>
   );
--- src/components/RenderMissions.js
+++ src/components/RenderMissions.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-function RenderMissions({mission, handleJoin}) {
+function RenderMissions({mission, handleJoin, handleLeave}) {
     const {mission_id, mission_name} = mission
   return (
     <div>
         id:{mission_id} |
         mission name: {mission_name}
         <button type='button' onClick={()=> handleJoin(mission_id)}>Join Mission</button>
+        <button type='button' onClick={()=> handleLeave(mission_id)}>Leave Mission</button>
     </div>
   )
 }
