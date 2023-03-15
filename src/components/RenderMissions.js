2629cae68139d80e0e45596f9b5325a3fadadfbc
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-03-15 19:36:00 -0400

    styling missions

--- src/components/RenderMissions.js
+++ src/components/RenderMissions.js
@@ -8,10 +8,10 @@ function RenderMissions({ mission, handleJoin, handleLeave }) {
   const { reserved } = mission;
   return (
     <div className="mission_container">
-      <h3>{name}</h3>
-      <div>{description}</div>
-      <div>{reserved ? <b className="cyan">Active Member</b> : <b className="gray">NOT A MEMBER</b>}</div>
-      <div>
+      <div className="mission-name"><h3>{name}</h3></div>
+      <div className="description">{description}</div>
+      <div className="status">{reserved ? <b className="cyan">Active Member</b> : <b className="gray">NOT A MEMBER</b>}</div>
+      <div className="toogle">
         {reserved ? (
           <button type="button" onClick={() => handleLeave(id)} className="red">Leave Mission</button>
         ) : (
