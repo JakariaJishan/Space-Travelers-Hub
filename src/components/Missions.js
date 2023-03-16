a460043ee31c441f38ca3338a3adbc070e3e6180
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-03-15 20:09:22 -0400

    styling mission table head

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { joinMissions, leaveMissions } from '../app/missions/missionsSlice';
 import RenderMissions from './RenderMissions';
+import '../style/missions.css';
 
 function Missions() {
   const missions = useSelector((state) => state.missions.missions);
@@ -15,7 +16,13 @@ function Missions() {
   };
 
   return (
-    <div>
+    <div className="otro">
+      <div className="mission_container">
+        <h2 className="table"> Mission</h2>
+        <h2 className="table"> Description</h2>
+        <h2 className="table"> Status</h2>
+        <h2 className="table"> </h2>
+      </div>
       {missions.map((mission) => (
         <RenderMissions
           key={mission.mission_id}
